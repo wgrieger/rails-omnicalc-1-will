24673e339f557f72a53617a1a822aef73eba7aad
Will Grieger <165510301+wgrieger@users.noreply.github.com>
2024-04-25 01:41:25 +0000

    done!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,9 @@ class PagesController < ApplicationController
   end
 
   def square_results
-    render({:template=>"square_results"})
+    @input_sqr=params.fetch(:number).to_f
+    @result_sqr= @input_sqr*@input_sqr
+    render({:template=>"squareresult"})
   end
 
   def squareroot
@@ -12,15 +14,22 @@ class PagesController < ApplicationController
   end
 
   def squareroot_results
-    render({:template=>"squareroot_results"})
+    @input_sqrt=params.fetch(:user_number).to_f
+@result_sqrt= Math.sqrt(@input_sqrt)
+    render({:template=>"squarerootresult"})
   end
 
   def random
+    
     render({:template=>"random"})
   end
 
   def random_results
-    render({:template=>"random_results"})
+    @input_min=params.fetch(:user_min).to_f
+@input_max=params.fetch(:user_max).to_f
+
+@random_number= rand(@input_min..@input_max)
+    render({:template=>"randomresult"})
   end
 
   def payment
@@ -28,6 +37,17 @@ class PagesController < ApplicationController
   end
   
   def payment_results
-    render({:template=>"payment_results"})
+    @user_apr=params.fetch(:user_apr).to_f
+@apr_input=sprintf("%.4f%%", @user_apr)
+@years_input_preconversion=params.fetch(:user_years).to_i
+@principal_input=params.fetch(:user_pv).to_f
+@years_input_months=@years_input_preconversion*12
+@display_dollar= @principal_input.to_fs(:currency)
+@r=@user_apr/100/12
+@part_denominator=(1+@r)**(-@years_input_months)
+
+@payment_output= (@r*@principal_input)/(1-@part_denominator)
+@payment_output_rounded=@payment_output.round(2).to_fs(:currency)
+    render({:template=>"paymentresult"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 get("/square/new", { :controller => "pages", :action => "square"})
 get("/", { :controller => "pages", :action => "square"})
-get("/square/results", { :controller => "pages", :action => "square_result"})
+get("/square/results", { :controller => "pages", :action => "square_results"})
 get("/square_root/new", { :controller => "pages", :action => "squareroot"})
-get("/square_root/results", { :controller => "pages", :action => "squareroot_result"})
+get("/square_root/results", { :controller => "pages", :action => "squareroot_results"})
 get("/random/new", { :controller => "pages", :action => "random"})
-get("/random/results", { :controller => "pages", :action => "random_result"})
+get("/random/results", { :controller => "pages", :action => "random_results"})
 get("/payment/new", { :controller => "pages", :action => "payment"})
-get("/payment/results", { :controller => "pages", :action => "payment_result"})
+get("/payment/results", { :controller => "pages", :action => "payment_results"})
 end
